4506b5d2a5eee40820f5a4a3b1d8a676471e90ec
Arielle Gordon <reallygordon@gmail.com>
2018-05-16 14:36:15 -0400

    added reducer functionality

--- game/index.js
+++ game/index.js
@@ -1,17 +1,17 @@
-import { Map } from 'immutable';
-import { createStore } from 'redux';
+import { Map } from "immutable";
+import { createStore } from "redux";
 
 const board = Map();
 
 const initialState = {
-  turn: 'X',
+  turn: "X",
   board: board
 };
 
-const MOVEX = 'MOVEX';
-const MOVEO = 'MOVEO';
+const MOVEX = "MOVEX";
+const MOVEO = "MOVEO";
 
-const MOVE = 'MOVE';
+const MOVE = "MOVE";
 // export const move = (type, position, player) => {
 //   return {
 //     type: type,
@@ -31,18 +31,18 @@ export const move = (player, position) => {
 
 export default function reducer(state = initialState, action) {
   // TODO
-  console.log('action: ', action);
-  console.log('state', state);
+  console.log("action: ", action);
+  console.log("state", state);
   ///if (state === {}) state = initialState;
   //const newState = Object.assign({}, state);
   switch (action.type) {
     case MOVE:
       //ternary for if x, make move x at position
-      console.log('oldboard:', board);
+      console.log("oldboard:", board);
       return {
-        board: board.setIn(action.position, action.turn),
+        board: state.board.setIn(action.position, action.turn),
 
-        turn: action.turn === 'X' ? 'O' : 'X'
+        turn: action.turn === "X" ? "O" : "X"
       };
     default:
       return state;
