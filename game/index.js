967decf23dff30886b753d0fc5324a851fcb2afe
Kaitlin Schaer <kaitlin.schaer@gmail.com>
2018-05-16 13:06:13 -0400

    why are we overwriting the board?

--- game/index.js
+++ game/index.js
@@ -1,6 +1,58 @@
-import {Map} from 'immutable'
+import { Map } from 'immutable';
+import { createStore } from 'redux';
 
-export default function reducer(state, action) {
+const board = Map();
+
+const initialState = {
+  turn: 'X',
+  board: board
+};
+
+const MOVEX = 'MOVEX';
+const MOVEO = 'MOVEO';
+
+const MOVE = 'MOVE';
+// export const move = (type, position, player) => {
+//   return {
+//     type: type,
+//     position: position,
+//     player: player
+//   };
+// };
+let player = board.turn;
+
+export const move = (player, position) => {
+  return {
+    type: MOVE,
+    turn: player,
+    position: position
+  };
+};
+
+export default function reducer(state = initialState, action) {
   // TODO
-  return state
+  console.log('action: ', action);
+  console.log('state', state);
+  ///if (state === {}) state = initialState;
+  //const newState = Object.assign({}, state);
+  switch (action.type) {
+    case MOVE:
+      //ternary for if x, make move x at position
+      console.log('oldboard:', board);
+      return {
+        board: board.setIn(action.position, action.turn),
+
+        turn: action.turn === 'X' ? 'O' : 'X'
+      };
+    default:
+      return state;
+  }
+  // switch(action.turn){
+  //   case X:
+  //   case O:
+  //   default:
+  //     retur
+  // }
 }
+
+// export const game = createStore(reducer);
